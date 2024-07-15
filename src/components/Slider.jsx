116c015f4c1c2755922bb25532d13c5b25deb21a
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-07-15 14:48:55 +0530

    upadetes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -41,7 +41,17 @@ const Slider = ({
 
   return (
     <div className="h-max pb-80 justify-center bg-blue-300" >
-      <div className='pt-14 ps-16'>
+      <div className='pt-5 ps-12 mb-5'>
+      <button 
+  className='bg-indigo-950 px-5 py-2 text-white rounded-md' 
+  onClick={() => window.location.href = 'https://novajobs.us/user/jobs-profile'}
+>
+  To Dashboard
+</button>
+
+      </div>
+      <div className='ps-16' >
+       
         <img src={logo}  className='w mb-10'style={{ width:'130px'}} alt="" />
       </div>
       <div className="px- py-3 flex ps-10">
